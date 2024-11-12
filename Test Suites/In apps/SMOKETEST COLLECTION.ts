ce1c222e04e7baab8314239524d907685bd27caa
jenkins <junkmailist0908@gmail,com>
2024-11-12 18:01:12 +0700

    Add again

--- Test Suites/In apps/SMOKETEST COLLECTION.ts
+++ Test Suites/In apps/SMOKETEST COLLECTION.ts
@@ -57,5 +57,25 @@
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/In apps/IOS/Regress new build</testSuiteEntity>
       </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Mobile</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>true</requireConfigurationData>
+            <runConfigurationData>
+               <entry>
+                  <key>deviceName</key>
+                  <value>iPhone iPhone 18.0.1</value>
+               </entry>
+               <entry>
+                  <key>deviceId</key>
+                  <value>00008030-000E1CE814FA402E</value>
+               </entry>
+            </runConfigurationData>
+            <runConfigurationId>iOS</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/In apps/IOS/Smoketest English</testSuiteEntity>
+      </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
