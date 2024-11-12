d891f37f7629159571de3a5ebc03979cc5678df6
jenkins <junkmailist0908@gmail,com>
2024-11-12 18:40:31 +0700

    testcommit

--- Test Suites/In apps/IOS/Smoketest English.ts
+++ Test Suites/In apps/IOS/Smoketest English.ts
@@ -5,10 +5,87 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>3fa8252d-13f4-4c9c-8244-4e6af147119b</testSuiteGuid>
+   <testCaseLink>
+      <guid>1721ef0b-93b1-4d62-beec-8bdc5a9cc0b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IOS/ENGLISH/SmokeTest/LA-IOS-ENG-0008-Login without OTP</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1b2467b5-f8d1-4552-9fcb-014960e817de</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IOS/ENGLISH/SmokeTest/LA-IOS-ENG-0009-Logout</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0344e60f-1709-4db7-b9a5-773bcdb6f4a3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IOS/ENGLISH/SmokeTest/LA-IOS-ENG-0007-Login Wihout OTP and change PIN</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d705a595-b2e9-4c82-8057-ef58c6df704f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IOS/ENGLISH/SmokeTest/LA-IOS-ENG-0006-Check balance and Bonus balance</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b7f2b90a-9277-4535-956f-c5fdafb07c58</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IOS/ENGLISH/SmokeTest/LA-IOS-ENG-0005-Click Dashboard</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9bf802fd-0201-4e61-9e97-a22063351031</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IOS/ENGLISH/SmokeTest/LA-IOS-ENG-0010-Profile check</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3989c0f5-558d-4875-b785-94ab785871d1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IOS/ENGLISH/SmokeTest/LA-IOS-ENG-0013-Cashout all</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>77b349d9-a3d9-45b9-bdc1-1c35c8a81179</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IOS/ENGLISH/SmokeTest/LA-IOS-ENG-0014-P2P</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>915dc4c2-045f-4868-8358-047fbda1bb26</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IOS/ENGLISH/SmokeTest/LA-IOS-ENG-0015-See All Check</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cd19caa2-393d-45c5-8a80-f47b379c45d3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IOS/ENGLISH/SmokeTest/LA-IOS-ENG-0004-Fav and re-do Fav</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b3812704-1f04-42e7-b78d-8fc5c25c9c27</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IOS/ENGLISH/SmokeTest/LA-IOS-ENG-0001-Change Language</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
